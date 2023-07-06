c6ff646f447f5d1f9b96c086695194b2bd4322d2
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-07-06 20:46:05 -0300

    Criado tela Detalhes da Refeição

--- lib/screens/tela_detalhe_refeicao.dart
+++ lib/screens/tela_detalhe_refeicao.dart
@@ -4,17 +4,103 @@ import '../models/refeicao.dart';
 class TelaDetalheRefeicao extends StatelessWidget {
   const TelaDetalheRefeicao({super.key});
 
+  Widget _criarTituloSecao(BuildContext context, String titulo) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        vertical: 10,
+      ),
+      child: Text(
+        titulo,
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
+    );
+  }
+
+  Widget _criarContainerSecao(Widget child) {
+    return Container(
+      width: double.infinity,
+      height: 250,
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          color: Colors.grey,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final refeicao = ModalRoute.of(context)!.settings.arguments as Refeicao;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(refeicao.titulo),
-      ),
-      body: const Center(
-        child: Text('Detalhes da refeição'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(refeicao.titulo),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  refeicao.imagemUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              _criarTituloSecao(context, 'Ingredientes'),
+              _criarContainerSecao(
+                ListView.builder(
+                  itemCount: refeicao.ingredientes.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color: Theme.of(context).colorScheme.secondary,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 5,
+                        ),
+                        child: Text(refeicao.ingredientes[index]),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              _criarTituloSecao(context, 'Passos'),
+              _criarContainerSecao(
+                ListView.builder(
+                  itemCount: refeicao.passos.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primary,
+                            child: Text('${index + 1}'),
+                          ),
+                          title: Text(refeicao.passos[index]),
+                        ),
+                        Divider(
+                          height: 1,
+                          color: (index + 1) != refeicao.passos.length
+                              ? Colors.black54
+                              : Colors.white,
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }

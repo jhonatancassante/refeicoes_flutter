166efa1d9be554113cef3b9870c0bb6289451fa3
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-07-06 09:02:46 -0300

    Configurando Grid

---
+++ lib/screens/tela_categorias.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TelaCategorias extends StatelessWidget {
+  const TelaCategorias({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: const <Widget>[],
+    );
+  }
+}

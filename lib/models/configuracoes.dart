62892322ae3981fec28d5fb50b4ab440f53a3e73
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-07-07 15:00:34 -0300

    Criada a classe Configuracoes

---
+++ lib/models/configuracoes.dart
@@ -0,0 +1,13 @@
+class Configuracoes {
+  bool semGlutem;
+  bool semLactose;
+  bool eVegano;
+  bool eVegetariano;
+
+  Configuracoes({
+    this.semGlutem = false,
+    this.semLactose = false,
+    this.eVegano = false,
+    this.eVegetariano = false,
+  });
+}

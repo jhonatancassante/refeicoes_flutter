3d7a38c8c33ff4936e51ca14a7bd16ac629ee23d
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-07-06 21:32:06 -0300

    Alterado método de abas

--- lib/screens/tela_abas.dart
+++ lib/screens/tela_abas.dart
@@ -2,8 +2,31 @@ import 'package:flutter/material.dart';
 import 'tela_categorias.dart';
 import 'tela_favoritos.dart';
 
-class TelaAbas extends StatelessWidget {
+class TelaAbas extends StatefulWidget {
   const TelaAbas({super.key});
+
+  @override
+  State<TelaAbas> createState() => _TelaAbasState();
+}
+
+class _TelaAbasState extends State<TelaAbas> {
+  int _indiceTelaSelecionada = 0;
+  final List<Map<String, Object>> _telas = [
+    {
+      'titulo': 'Lista de Categorias',
+      'tela': const TelaCategorias(),
+    },
+    {
+      'titulo': 'Meus Favoritos',
+      'tela': const TelaFavoritos(),
+    },
+  ];
+
+  _selecionaTela(int index) {
+    setState(() {
+      _indiceTelaSelecionada = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,26 +34,24 @@ class TelaAbas extends StatelessWidget {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Vamos Cozinhar?'),
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favoritos',
-              ),
-            ],
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.white70,
-          ),
+          title: Text(_telas[_indiceTelaSelecionada]['titulo'] as String),
         ),
-        body: const TabBarView(
-          children: [
-            TelaCategorias(),
-            TelaFavoritos(),
+        body: _telas[_indiceTelaSelecionada]['tela'] as Widget,
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selecionaTela,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).colorScheme.secondary,
+          currentIndex: _indiceTelaSelecionada,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: 'Categorias',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favoritos',
+            ),
           ],
         ),
       ),
